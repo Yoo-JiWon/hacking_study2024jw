4927ac909c1347b773ff4c376019b6c8dcbdd9e0
Yoo-JiWon <220246055@sungshin.ac.kr>
2024-12-06 13:06:08 +0100

    3

--- system/nc-sample/start.sh
+++ system/nc-sample/start.sh
@@ -2,5 +2,5 @@
 
 while :
 do
-    su -c "exec socat TCP-LISTEN:7001,reuseaddr,fork EXEC:'/challenge/free_shell.py,stderr'" - challenge;
+    su -c "exec socat TCP-LISTEN:7201,reuseaddr,fork EXEC:'/challenge/free_shell.py,stderr'" - challenge;
 done
